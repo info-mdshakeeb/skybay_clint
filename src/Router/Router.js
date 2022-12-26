c8a1aae2cdc94f403b169fbe4c146c73411251ec
ShAkEeB <shakeeb@ShAkEeBs-MacBook-Pro.local>
2022-12-27 02:22:04 +0600

    router satup complite

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,15 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import About from "../Components/About/About";
 import Home from "../Components/Home/Home";
-import NavBar from "../Components/NavBar";
+import Media from "../Components/Media/Media";
+import Message from "../Components/Message/Message";
+import Main from "../Layout/Main";
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <NavBar />,
+        element: <Main />,
         children: [
             { path: '/', element: <Home /> },
             { path: '/home', element: <Home /> },
+            { path: '/Media', element: <Media /> },
+            { path: '/Message', element: <Message /> },
+            { path: '/About', element: <About /> },
         ]
-
     }
 ])
